1db839124e073de21bcf733e2a745584d66eb387
CesarTest <cesar.electronic.engineer@gmail.com>
2020-02-28 17:35:36 +0100

    Views Missing

---
+++ src/views/BookView.php
@@ -0,0 +1,79 @@
+<?php
+use Monolog\Logger;
+
+/**
+ *
+ * @author cesar
+ *        
+ */
+class BookView extends View
+{
+    
+    //--------------------------------
+    //       PRIVATE METHODS
+    //--------------------------------
+    // 1.- Form Fields
+    protected $contact;
+    protected $labels;
+    
+    /**
+     * @return multitype:multitype: 
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * @param multitype:multitype:  $contact
+     */
+    public function setContact(array $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     *
+     * @param Logger $log
+     */
+    public function __construct(Logger $log = null)
+    {
+        parent::__construct($log = null);
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see ObjetoWeb::init()
+     */
+    public function init(array $parentProperties=[])
+    {
+        $log_header=$this->line_header . __METHOD__ ."()] - ";
+        try {
+            
+            // 0.- Trace Entry
+            $this->debug($log_header . "INIT class=[$this->clase]");
+            
+            // 1.- Set Child Properties
+            $this->labels=[ 'firstname'  => 'Name' 
+                           , 'lastname'  => 'Surname'
+                           , 'address'   => 'Address'
+                           , 'email'     => 'eMail'
+                           , 'phone'     => 'Phone'
+                          ];
+            
+            // 2.- Set Parent Properties
+            parent::init($parentProperties);
+            
+        } catch (Exception $e) {
+            $this->treatException($e
+                , $log_header . "ADDRESS BOOK LISTING"
+                );
+        } catch (Error $e) {
+            $this->treatError($e
+                , $log_header . "ADDRESS BOOK LISTING"
+                );
+        }
+    }
+}
+
---
+++ src/views/ContactView.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ *
+ * @author Cesar Delgado
+ *        
+ */
+use Monolog\Logger;
+
+class ContactView extends View
+{
+
+    //--------------------------------
+    //       PRIVATE METHODS
+    //--------------------------------
+    // 1.- Form Fields
+    protected $fields;
+    
+
+    //--------------------------------
+    //       PUBLIC METHODS
+    //--------------------------------
+    
+    /**
+     *
+     * @param Logger $log
+     */
+    public function __construct(Logger $log = null)
+    {
+        parent::__construct($log = null);
+    }
+
+    /**
+     * 
+     * {@inheritDoc}
+     * @see ObjetoWeb::init()
+     */
+    public function init(array $parentProperties=[])
+    {
+        $log_header=$this->line_header . __METHOD__ ."()] - ";
+        try {
+ 
+            // 0.- Trace Entry
+            $this->debug($log_header . "INIT class=[$this->clase]");
+            
+            // 1.- Set Child Properties
+            $this->fields=[ 'firstname' => ['value'=>'', 'type'=>'varchar', 'min'=>'1', 'max'=>'50', 'label'=>'First Name' ] 
+                    , 'lastname'  => ['value'=>'', 'type'=>'varchar', 'min'=>'1', 'max'=>'50', 'label'=>'Surname' ]
+                    , 'address'   => ['value'=>'', 'type'=>'varchar', 'min'=>'1', 'max'=>'255', 'label'=>'Address' ]
+                    , 'email'     => ['value'=>'', 'type'=>'varchar', 'min'=>'0', 'max'=>'50', 'label'=>'eMail' ]
+                    , 'phone'     => ['value'=>'', 'type'=>'varchar', 'min'=>'0', 'max'=>'20', 'label'=>'Phone' ]
+            ];
+            
+            // 2.- Set Parent Properties
+            parent::init($parentProperties);
+            
+        } catch (Exception $e) {
+            $this->treatException($e
+                , $log_header . "NEWz ADDRESS FORM"
+                );
+        } catch (Error $e) {
+            $this->treatError($e
+                , $log_header . "NEW ADDRESS FORM"
+                );
+        }
+    }
+
+    //--------------------------------
+    //       GETTER / SETTER
+    //--------------------------------
+    /**
+     * @return \src\models\beans\Address
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+    
+    /**
+     * @param \src\models\beans\Address $address
+     */
+    public function setFields($address)
+    {
+        $this->fields = $address;
+    }
+    
+}
+
